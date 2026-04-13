f6edec76304d5b76d48378198359ebc6328cbdc6
Thyra-S <thysesimons@gmail.com>
2026-04-13 11:05:30 -0600

    diffuse materials

--- Software Ray Tracer/color.h
+++ Software Ray Tracer/color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include <glm.hpp>
 
 using color = glm::vec3;
@@ -12,9 +13,10 @@ void write_color(std::ostream& out, const color& pixel_color)
 	auto b = pixel_color.b;
 
 	// Translate the [0,1] component values to the byte range [0,255].
-	int rbyte = int(255.999 * r);
-	int gbyte = int(255.999 * g);
-	int bbyte = int(255.999 * b);
+	static const interval intensity(0.000f, 0.999f);
+	int rbyte = int(256 * intensity.clamp(r));
+	int gbyte = int(256 * intensity.clamp(g));
+	int bbyte = int(256 * intensity.clamp(b));
 
 	// Write out the pixel color components.
 	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
--- Software Ray Tracer/rtweekend_commons.h
+++ Software Ray Tracer/rtweekend_commons.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 
 // C++ Std usings
@@ -24,14 +25,41 @@ inline float degrees_to_radians(float degrees) {
 	return degrees * pi / 180.0f;
 }
 
+inline float random_float() {
+	static std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
+
+inline float random_float(float min, float max) {
+	// Returns a random real in [min,max).
+	return min + (max - min) * random_float();
+}
+
+
+
+
 // Common Headers
 
 #include "color.h"
 #include "interval.h"
 #include "ray.h"
 #include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
 
 using color = glm::vec3;
 using POINT = glm::vec3;
+
+inline glm::vec3 random_unit_vector() {
+	return glm::sphericalRand(1.0f);
+}
+
+inline glm::vec3 random_on_hemisphere(const glm::vec3& normal) {
+	glm::vec3 on_unit_sphere = random_unit_vector();
+	if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+		return on_unit_sphere;
+	else
+		return -on_unit_sphere;
+}
 
 #endif
